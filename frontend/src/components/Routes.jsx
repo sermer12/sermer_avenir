16ece946ef6d0a56c8cf7cc35c29bc7857fd3b2c
pavel <pavelnesvel@yahoo.fr>
2024-01-22 20:41:27 +0100

    animation navbar lorsquon est actif sur un menu

--- frontend/src/components/Routes.jsx
+++ frontend/src/components/Routes.jsx
@@ -1,21 +1,49 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
-import "../styles/root.css"
+import "../styles/root.css";
 
 const Routes = () => {
   return (
     <>
       <header className="hearder_root">
-        <div className="logo_sumer">
-          LOGO
-        </div>
+        <div className="logo_sumer">LOGO</div>
         <nav>
-          <NavLink to="/">HomePage</NavLink>
-          <NavLink to="/Qui sommes-nous">Qui sommes-nous</NavLink>
-          <NavLink to="/Nos formations">Nos formations</NavLink>
-          <NavLink to="/Nos actualites">Nos actualités</NavLink>
-          <NavLink to="/dashboard">Dashboard</NavLink>
-          <NavLink to="/Contact">Contact</NavLink>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            HomePage
+          </NavLink>
+          <NavLink
+            to="/Qui sommes-nous"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            Qui sommes-nous
+          </NavLink>
+          <NavLink
+            to="/Nos formations"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            Nos formations
+          </NavLink>
+          <NavLink
+            to="/Nos actualites"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            Nos actualités
+          </NavLink>
+          <NavLink
+            to="/dashboard"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            Dashboard
+          </NavLink>
+          <NavLink
+            to="/Contact"
+            className={({ isActive }) => (isActive ? "is_active" : "")}
+          >
+            Contact
+          </NavLink>
         </nav>
       </header>
       <div className="container">
