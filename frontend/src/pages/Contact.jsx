24632d92f4ba8f76bb7acebddc468d8febb3aaee
Samy ZIREM <samyzirem7@hotmail.com>
2024-02-08 23:07:08 +0100

    Ajout du squelette de la page contact

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import '../styles/contact.css';
 
-const Contact = () => {
-  return <div>Contact pavel</div>;
-};
+function Contact() {
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [subscribeNewsletter, setSubscribeNewsletter] = useState(false);
+
+  const handleCheckboxChange = (e) => {
+    setSubscribeNewsletter(e.target.checked);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      setShowSuccessMessage(true);
+      e.target.reset();
+      setTimeout(() => {
+        setShowSuccessMessage(false);
+      }, 1500);
+    });
+  };
+
+  return (
+    <div className="contact-page">
+      <h2>CONTACTEZ-NOUS</h2>
+      <div className="contact-info">
+        <p>Veuillez remplir ce formulaire afin de nous contacter</p>
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="nom">Nom :</label>
+            <input type="text" id="nom" name="nom" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="prenom" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email :</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Message :</label>
+            <textarea id="message" name="message" rows="5" required></textarea>
+          </div>
+          <div className="form-group">
+            <label>
+              <span>Recevoir les newsletters de l'association</span>
+              <input className="checkB" type="checkbox" checked={subscribeNewsletter} onChange={handleCheckboxChange} />
+            </label>
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
+      </div>
+      {showSuccessMessage && (
+        <div className="success-message">
+          Message envoyé avec succès !
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Contact;
