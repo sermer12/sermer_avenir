142017c35b0645d602c4ee06b4a67b8d806aa967
SamyZIREM <91314042+SamyZIREM@users.noreply.github.com>
2024-03-22 10:35:43 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    //Remplacez l'url origin par le local front 
+    origin: "http://localhost:5173",
     credentials: true,
     optionsSuccessStatus: 200,
   })
