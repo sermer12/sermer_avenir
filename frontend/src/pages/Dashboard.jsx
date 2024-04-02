5b862840ce4ad6b62b9fd34105fe9a09ceaec61e
pavel <pavelnesvel@yahoo.fr>
2024-04-02 15:19:40 +0200

    fix: premiere etape detition de la formation

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import { AdminNavigation } from "../components/AdminRoute/AdminNavigation";
 const Dashboard = () => {
   return (
     <div>
-      <h1>Administration des formations</h1>
+      <h1  className="title-amin">Administration des formations</h1>
       <AdminNavigation />
     </div>
   );
--- frontend/src/pages/EditFormation.jsx
+++ frontend/src/pages/EditFormation.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { FormationsContext } from "../context/FormationsContext";
+import { getFormations } from "../repository/FormationsRepository";
 
 const EditFormation = () => {
-  return <div>EditFormation</div>;
+  const [appState, setAppState] = useContext(FormationsContext);
+  useEffect(() => {
+    handleGetFormations();
+  }, []);
+
+  const handleGetFormations = () => {
+    getFormations().then((resp) => {
+      setAppState({
+        ...appState,
+        formations: resp.data,
+      });
+    });
+  };
+
+  const dateFormater = (chaine) => {
+    let newDate = new Date(chaine).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    });
+    return newDate;
+  };
+
+  return (
+    <div className="formations-container-wrapper">
+      <div className="formations-container">
+        <h1>Vous pouvez editer des formations</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Nom</th>
+              <th>Lieu</th>
+              <th>Description</th>
+              <th>Editer</th>
+              <th>Supprimer</th>
+            </tr>
+          </thead>
+          <tbody>
+            {appState.formations.map((formation, index) => (
+              <tr key={index}>
+                <td className="formation-periode">
+                  {dateFormater(formation.date)} {""}{" "}
+                  <strong>
+                    {" "}
+                    {formation.heure && (
+                      <span className="hour-periode">
+                        {" "}
+                        <span className="periode-a">à </span>
+                        {formation.heure}
+                      </span>
+                    )}{" "}
+                  </strong>
+                </td>
+                <td>{formation.name}</td>
+                <td>{formation.place}</td>
+                <td>{formation.description}</td>
+                <td>
+                  <button className="download">&#9999;&#65039;</button>
+                </td>
+                <td>
+                  <button className="signup"> &#128465;&#65039;</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default EditFormation;
