40ff9a2e2ac41ae03f3b2f91383a7c39a5b727a2
Lilecaz <lilecaz0090@gmail.com>
2024-04-02 15:24:56 +0200

    changement dans la page qui-sommes nous

--- frontend/src/pages/QuiSommesNous.jsx
+++ frontend/src/pages/QuiSommesNous.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import '../styles/presentation.css'
+import React from 'react';
+import '../styles/presentation.css';
 
 const QuiSommesNous = () => {
+  //TODO : mettre des images 
   return (
     <div className='quiSommesNous_container'>
       <h1>Qui sommes-nous</h1>
       <div className="quiSommesNous_content">
-
+        <p>
+          "Semer l’Avenir" est une association fondée en accord avec la loi du 1er juillet 1901 et le décret du 16 août 1901, ainsi que la loi n°2014‐856 du 31 juillet 2014 relative à l’économie sociale et solidaire.
+        </p>
+        <p>
+          Notre objectif est d'accompagner la résilience d'un territoire à travers l'émergence d'initiatives locales, écologiques, sociales et solidaires. Nous promouvons les pratiques respectueuses de la nature et de l'humain, en sensibilisant, accompagnant et formant aux métiers de la transition écologique.
+        </p>
+        <p>
+          Nous coopérons activement avec tous les acteurs du territoire pour contribuer au développement humain dans la cité. Notre action vise à créer de nouveaux imaginaires et récits pour semer les graines d'une transformation sociétale, promouvoir les initiatives éco-citoyennes, et créer des espaces ouverts dans le respect de chacun.
+        </p>
+        <p>
+          Nos membres, qu'ils soient d'honneur, bienfaiteurs ou actifs, ont tous une voix égale dans nos processus de décision. Nous nous engageons à respecter nos valeurs fondamentales, telles que l'éducation à la citoyenneté et à l'environnement, et à promouvoir la culture dans toutes nos actions.
+        </p>
+        <p>
+          Le siège social de notre association est situé à Créative Factory, 32 Boulevard de la Muette 95140, Garges-lès-Gonesse. Pour plus d'informations sur nos activités et nos actions, n'hésitez pas à nous contacter.
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuiSommesNous
+export default QuiSommesNous;
