1bb24fc89e8e78c41b4f8758480862746eeed8db
pavel <pavelnesvel@yahoo.fr>
2024-04-08 11:10:17 +0200

    fix: css

--- frontend/src/pages/QuiSommesNous.jsx
+++ frontend/src/pages/QuiSommesNous.jsx
@@ -1,27 +1,116 @@
-import React from 'react';
-import '../styles/presentation.css';
+import React from "react";
+import "../styles/presentation.css";
 
 const QuiSommesNous = () => {
-  //TODO : mettre des images 
+  //TODO : mettre des images
   return (
-    <div className='quiSommesNous_container'>
+    <div className="quiSommesNous_container">
       <h1>Qui sommes-nous</h1>
       <div className="quiSommesNous_content">
         <p>
-          "Semer l’Avenir" est une association fondée en accord avec la loi du 1er juillet 1901 et le décret du 16 août 1901, ainsi que la loi n°2014‐856 du 31 juillet 2014 relative à l’économie sociale et solidaire.
+          "Semer l’Avenir" est une association fondée en accord avec la loi du
+          1er juillet 1901 et le décret du 16 août 1901, ainsi que la loi
+          n°2014‐856 du 31 juillet 2014 relative à l’économie sociale et
+          solidaire.
         </p>
         <p>
-          Notre objectif est d'accompagner la résilience d'un territoire à travers l'émergence d'initiatives locales, écologiques, sociales et solidaires. Nous promouvons les pratiques respectueuses de la nature et de l'humain, en sensibilisant, accompagnant et formant aux métiers de la transition écologique.
+          Notre objectif est d'accompagner la résilience d'un territoire à
+          travers l'émergence d'initiatives locales, écologiques, sociales et
+          solidaires. Nous promouvons les pratiques respectueuses de la nature
+          et de l'humain, en sensibilisant, accompagnant et formant aux métiers
+          de la transition écologique.
         </p>
         <p>
-          Nous coopérons activement avec tous les acteurs du territoire pour contribuer au développement humain dans la cité. Notre action vise à créer de nouveaux imaginaires et récits pour semer les graines d'une transformation sociétale, promouvoir les initiatives éco-citoyennes, et créer des espaces ouverts dans le respect de chacun.
+          Nous coopérons activement avec tous les acteurs du territoire pour
+          contribuer au développement humain dans la cité. Notre action vise à
+          créer de nouveaux imaginaires et récits pour semer les graines d'une
+          transformation sociétale, promouvoir les initiatives éco-citoyennes,
+          et créer des espaces ouverts dans le respect de chacun.
         </p>
         <p>
-          Nos membres, qu'ils soient d'honneur, bienfaiteurs ou actifs, ont tous une voix égale dans nos processus de décision. Nous nous engageons à respecter nos valeurs fondamentales, telles que l'éducation à la citoyenneté et à l'environnement, et à promouvoir la culture dans toutes nos actions.
+          Nos membres, qu'ils soient d'honneur, bienfaiteurs ou actifs, ont tous
+          une voix égale dans nos processus de décision. Nous nous engageons à
+          respecter nos valeurs fondamentales, telles que l'éducation à la
+          citoyenneté et à l'environnement, et à promouvoir la culture dans
+          toutes nos actions.
         </p>
         <p>
-          Le siège social de notre association est situé à Créative Factory, 32 Boulevard de la Muette 95140, Garges-lès-Gonesse. Pour plus d'informations sur nos activités et nos actions, n'hésitez pas à nous contacter.
+          Le siège social de notre association est situé à Créative Factory, 32
+          Boulevard de la Muette 95140, Garges-lès-Gonesse. Pour plus
+          d'informations sur nos activités et nos actions, n'hésitez pas à nous
+          contacter.
         </p>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          voluptate nobis debitis. Neque, vitae quis nostrum modi nihil quidem
+          vero distinctio at nisi ea doloremque dolorem facere amet ipsa
+          deserunt? Architecto voluptatem totam pariatur officiis facilis libero
+          delectus ducimus velit doloribus aut, voluptas officia praesentium
+          quasi quis minima inventore error laborum ea esse! Cumque iusto
+          repellendus enim debitis officiis sint. Ad vero culpa voluptas hic
+          odit tempora ex! Optio minima officiis provident quas magni tenetur
+          dolore, nemo iste doloremque laborum ducimus aspernatur facilis,
+          voluptatum sit non hic possimus accusantium. Dolore. Est corporis
+          quod, illo harum quisquam perspiciatis earum beatae perferendis
+          blanditiis saepe iure nihil sed quo ipsa omnis inventore nisi cumque
+          repellendus, ea fugiat, repellat ut ex modi. Maxime, nostrum. Deserunt
+          distinctio quod animi eveniet aperiam, ex incidunt, molestias sit
+          aliquam voluptatum possimus ab? Unde eius ducimus eos, expedita atque
+          rem, quibusdam tenetur autem consequatur, dignissimos deleniti
+          laboriosam aliquid deserunt! Nostrum mollitia nobis natus excepturi
+          perspiciatis, quasi quis ab debitis totam quam sapiente commodi
+          ratione atque? Harum perspiciatis recusandae dolore tempore sequi
+          nesciunt, beatae magni, delectus quasi veritatis praesentium
+          molestiae? Assumenda excepturi, eveniet, provident ipsa similique
+          fugiat tempore libero et ducimus quibusdam repellendus quo aspernatur
+          harum laudantium minus distinctio qui. Saepe corporis architecto totam
+          quibusdam voluptas alias a explicabo nisi! Nisi, consequuntur, dolore
+          tenetur magnam, ratione porro accusantium corrupti debitis numquam
+          totam ab possimus dicta alias vero quam odio laboriosam. Itaque quae
+          ducimus sequi accusamus quisquam suscipit ad nemo deserunt!
+          Architecto, totam odio mollitia nesciunt rerum pariatur consequatur
+          voluptatem dolor illum odit quaerat facere tenetur corporis optio
+          fugiat? Id voluptatibus voluptates porro ipsa quibusdam! Veniam
+          repellat atque ipsum suscipit dicta. Hic consectetur veritatis,
+          debitis dolore corporis consequuntur sequi expedita fuga amet illo
+          deserunt alias ipsum quas tempora voluptatum non officiis placeat quia
+          iure. Laboriosam voluptas delectus amet ratione, repudiandae
+          dignissimos! Totam ducimus illo non, adipisci nulla necessitatibus
+          quaerat quia nostrum, ea molestias assumenda nemo similique
+          accusantium veritatis nisi dolor nesciunt. Enim pariatur explicabo
+          omnis nihil eveniet, nemo eos nesciunt vitae! Asperiores, obcaecati
+          excepturi! Fuga nulla sunt, soluta accusamus qui expedita atque in cum
+          enim dignissimos pariatur rem impedit natus porro possimus facere
+          dolorem omnis ullam eaque veritatis nostrum. Aspernatur, nostrum!
+          Exercitationem vitae et aliquid ut amet itaque aut earum, voluptate
+          quibusdam explicabo sunt nesciunt quasi at deleniti labore fugiat
+          repellendus corrupti, harum nisi, doloribus laborum atque? Ullam et
+          qui quod! Nulla eaque quis quia hic, nostrum, explicabo modi
+          repellendus tempora ea alias sit dolores corporis! Facere nostrum eum
+          sequi! At est, dolorum sequi pariatur a dolore accusamus voluptate
+          iure soluta! Ipsa voluptatem in laudantium itaque quibusdam ab officia
+          nemo dolorem, libero repellat facere sed laborum, cumque deleniti
+          possimus. Placeat odit architecto sequi doloremque explicabo inventore
+          nemo sint a unde tenetur? Animi quia natus sint id, officia fuga
+          dignissimos voluptatem, blanditiis in nam doloribus facilis,
+          architecto laborum vero accusantium similique necessitatibus maiores
+          soluta atque sit. Vel id quisquam rem a debitis? Aut magnam non
+          facilis saepe enim molestias officia nihil facere totam veniam.
+          Dolorum veniam praesentium dolore commodi ipsum rerum officiis autem,
+          inventore, unde laboriosam tempora maxime voluptate natus corrupti
+          saepe. Deleniti ullam amet voluptatibus. Reiciendis laudantium
+          distinctio perspiciatis asperiores velit aut unde voluptatum pariatur
+          dolores, suscipit, aspernatur exercitationem laborum quibusdam
+          obcaecati nobis, atque hic magni praesentium accusantium provident
+          fuga laboriosam? Incidunt vitae placeat earum, doloremque dicta
+          tempora! Voluptas, fugiat? Delectus quam architecto quasi aspernatur
+          quo adipisci laborum pariatur repudiandae reprehenderit nam deserunt
+          magni atque explicabo ratione, possimus inventore quae! Non? Excepturi
+          consectetur sunt atque ut repudiandae dicta accusantium recusandae
+          aspernatur! Minus perspiciatis dolores assumenda tempore quidem, magni
+          debitis inventore soluta laborum iure saepe enim autem provident
+          praesentium voluptatem quaerat minima.
+        </h1>
       </div>
     </div>
   );
