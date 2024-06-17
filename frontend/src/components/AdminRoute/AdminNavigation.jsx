585c19b15aa550d74b0d8bd0ede107dfc489371b
Lilecaz <lilecaz0090@gmail.com>
2024-06-17 23:52:18 +0200

    refonte UI dashboard

--- frontend/src/components/AdminRoute/AdminNavigation.jsx
+++ frontend/src/components/AdminRoute/AdminNavigation.jsx
@@ -6,9 +6,9 @@ export const AdminNavigation = () => {
   return (
     <div className="dashboard-container">
       <nav className="dashboard-nav">
-        <NavLink to="/dashboard">ajouter_une_Formation</NavLink>
-        <NavLink to="/dashboard/editformation">Editer_une_Formation</NavLink>
-        <NavLink to="/dashboard/editfooter">Editer_le_Footer</NavLink>
+        <NavLink to="/dashboard">ajouter une Formation</NavLink>
+        <NavLink to="/dashboard/editformation">Editer une Formation</NavLink>
+        <NavLink to="/dashboard/editfooter">Editer le Footer</NavLink>
       </nav>
 
       <div className="dashboard-content">
